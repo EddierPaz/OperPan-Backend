cb78003702a7c9ba1c05cd0464af79ab4a6e0544
Eddier Paz <eddierpaz9@gmail.com>
2026-07-29 10:52:35 -0500

    1.0.6 y 2.0.5 - feat/global: Configuración de botones / responsive

--- static/js/login/login.js
+++ static/js/login/login.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const togglePassword = document.querySelector('#togglePassword');
     const passwordInput = document.querySelector('#passwordInput');
 
@@ -7,10 +7,24 @@ document.addEventListener("DOMContentLoaded", function() {
             // Cambia el tipo de input entre texto y contraseña
             const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
             passwordInput.setAttribute('type', type);
-            
+
             // Alterna las clases del icono del ojo
             this.classList.toggle('bi-eye');
             this.classList.toggle('bi-eye-slash');
         });
+    }
+
+    const message = document.getElementById("globalMessage");
+
+    if (message) {
+        setTimeout(() => {
+            message.classList.remove("show");
+            message.classList.add("hide");
+
+            setTimeout(() => {
+                message.remove();
+            }, 400);
+
+        }, 3000);
     }
 });
